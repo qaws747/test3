54b802992520c567e0e3854530b8d178563be81d
songahyun <songahyun@localhost>
2020-07-06 01:22:21 +0900

    7/6forest
    forest edit

--- ujeongri2/src/ujeongri/com/gh/game/view/ForestView.java
+++ ujeongri2/src/ujeongri/com/gh/game/view/ForestView.java
@@ -40,8 +40,11 @@ public class ForestView extends JFrame implements Runnable, KeyListener {
 
 	public ForestView() {
 		super("우 - 정 - 리");
+		this.setLayout(null);
+		
 		this.setBounds(0, 0, 1440, 900);
 		this.forest();
+		this.start();
 		this.setVisible(true);
 		this.setFocusable(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -49,45 +52,50 @@ public class ForestView extends JFrame implements Runnable, KeyListener {
 
 	public void forest() {
 		// 패널 위치값 초기화
-		panel.setLayout(null);
+//		panel.setLayout(null);
 		// 패널 투명하게 없어도 되네
 // panel.setOpaque(false);
 
 		// 배경 이미지 만들고 라벨에 붙임
-//		Image background = new ImageIcon("src/images/forest.png").getImage().getScaledInstance(1440, 900, 0);
-//		bgLabel = new JLabel(new ImageIcon(background));
-//		bgLabel.setBounds(0, 0, 1440, 900);
+		Image background = new ImageIcon("src/images/forest.png").getImage().getScaledInstance(1440, 900, 0);
+		bgLabel = new JLabel(new ImageIcon(background));
+		bgLabel.setBounds(0, 0, 1440, 900);
 
 		// 빱이 이미지 만들고 라벨에 붙임
 //		Image img = new ImageIcon("src/images/wangnunramgi/ppap.png").getImage().getScaledInstance(150, 392, 0);
 //		papLabel = new JLabel(new ImageIcon(img));
 //		papLabel.setBounds(715, 10, 44, 98);
-
 		// 패널에 라벨들 붙임
-		panel.add(papLabel);
+//		panel.add(papLabel);
 		panel.add(bgLabel);
 
 		// this에 패널 붙임
 		this.add(panel);
 	}
 	
-	public void init() {
-		// 캐랙터 시작 위치
+//	public void init() {
+//		// 캐랙터 시작 위치
+//		x = 450;
+//		y = 550;
+//
+//		moveStatus = 2;
+////케릭터가 시작할때 바라보는 방향은 아래쪽입니다.
+//// 0 : 위쪽, 1 : 오른쪽, 2 : 아래쪽, 3 : 왼쪽
+//
+//	}
+
+	public void start() { // 기본적인 명령처리
 		x = 450;
 		y = 550;
 
 		moveStatus = 2;
-//케릭터가 시작할때 바라보는 방향은 아래쪽입니다.
-// 0 : 위쪽, 1 : 오른쪽, 2 : 아래쪽, 3 : 왼쪽
-
-	}
-
-	public void start() { // 기본적인 명령처리
-
+		
 		this.addKeyListener(this);
 		th = new Thread(this);
 		th.start();
+		
 	}
+
 	public void run() { // 스레드 메소드, 무한 루프
 		while (true) {
 			try {
@@ -101,32 +109,27 @@ public class ForestView extends JFrame implements Runnable, KeyListener {
 			}
 		}
 	}
+
+	
 	public void paint(Graphics g) { // 더블버퍼링을 사용합니다.
 		// 캐릭터가 사라지는 지점을 만든다.
 		buffimg = createImage(1440, 900);
 		gc = buffimg.getGraphics();
 		update(g);
-		
 	}
 
 	public void update(Graphics g) {
-		// 더블 버퍼링을 이용해 버퍼에 그려진것을 가져옵니다.
+//더블 버퍼링을 이용해 버퍼에 그려진것을 가져옵니다.
 		DrawImg();
-		back(g);
-		g.drawImage(buffimg, 0, 0, papLabel);
-		
+		g.drawImage(buffimg, 0, 0, this);
 	}
 	
-	public void back(Graphics g) {
-		g.drawImage(background, 0, 0, bgLabel);
-	}
-
+	
 	public void DrawImg() {
 		MoveImage(img, x, y, 53, 98);
 //케릭터를 걸어가게 만들기 위해 추가로 만든 메소드 입니다.
-		
-		
 	}
+	
 
 	public void MoveImage(Image img, int x, int y, int width, int height) {
 //케릭터 이미지, 케릭터 위치, 케릭터 크기를 받습니다.
@@ -159,75 +162,71 @@ public class ForestView extends JFrame implements Runnable, KeyListener {
 		}
 	}
 
+	public void keyProcess() {
+//여기서는 단순 케릭터가 이동하는 좌표 말고도
+//케릭터의 움직임 여부및 방향을 체크 합니다.
+		playerMove = false;
 
-		public void keyProcess() {
-			// 여기서는 단순 케릭터가 이동하는 좌표 말고도
-			// 케릭터의 움직임 여부및 방향을 체크 합니다.
-			playerMove = false;
-
-			if (keyUp) {
-				playerMove = true;
-				y -= 8;
-				moveStatus = 0;
-			}
-
-			if (keyDown) {
-				y += 8;
-				moveStatus = 2;
-				playerMove = true;
-			}
-
-			if (keyLeft) {
-				x -= 8;
-				moveStatus = 3;
-				playerMove = true;
-			}
-
-			if (keyRight) {
-				x += 8;
-				moveStatus = 1;
-				playerMove = true;
-			}
+		if (keyUp) {
+			playerMove = true;
+			y -= 8;
+			moveStatus = 0;
 		}
 
-		@Override
-		public void keyTyped(KeyEvent e) {
+		if (keyDown) {
+			y += 8;
+			moveStatus = 2;
+			playerMove = true;
 		}
 
-		@Override
-		public void keyPressed(KeyEvent e) {
-			switch (e.getKeyCode()) {
-			case KeyEvent.VK_LEFT:
-				keyLeft = true;
-				break;
-			case KeyEvent.VK_RIGHT:
-				keyRight = true;
-				break;
-			case KeyEvent.VK_UP:
-				keyUp = true;
-				break;
-			case KeyEvent.VK_DOWN:
-				keyDown = true;
-				break;
-			}
+		if (keyLeft) {
+			x -= 8;
+			moveStatus = 3;
+			playerMove = true;
 		}
 
-		@Override
-		public void keyReleased(KeyEvent e) {
-			switch (e.getKeyCode()) {
-			case KeyEvent.VK_LEFT:
-				keyLeft = false;
-				break;
-			case KeyEvent.VK_RIGHT:
-				keyRight = false;
-				break;
-			case KeyEvent.VK_UP:
-				keyUp = false;
-				break;
-			case KeyEvent.VK_DOWN:
-				keyDown = false;
-				break;
-			}
-
+		if (keyRight) {
+			x += 8;
+			moveStatus = 1;
+			playerMove = true;
 		}
+	}
+
+	public void keyPressed(KeyEvent e) {
+
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_LEFT:
+			keyLeft = true;
+			break;
+		case KeyEvent.VK_RIGHT:
+			keyRight = true;
+			break;
+		case KeyEvent.VK_UP:
+			keyUp = true;
+			break;
+		case KeyEvent.VK_DOWN:
+			keyDown = true;
+			break;
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_LEFT:
+			keyLeft = false;
+			break;
+		case KeyEvent.VK_RIGHT:
+			keyRight = false;
+			break;
+		case KeyEvent.VK_UP:
+			keyUp = false;
+			break;
+		case KeyEvent.VK_DOWN:
+			keyDown = false;
+			break;
+		}
+	}
+
+	public void keyTyped(KeyEvent e) {
+	}
 }
